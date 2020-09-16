0ee96a0b7606d189ffb6e96f35468338a0f98d16
Bryn <bschulha@ualberta.ca>
2020-09-16 21:16:38 +0000

    Made a func to count num of characters in a string

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  }
+ };
+
+ const countLetters = function(stringInput) {
+  const results = {};
+
+  for (const letter of stringInput) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+}
+
+countLetters ("lighthouse in the house");
+
+countLetters ("bryn is so cool");
