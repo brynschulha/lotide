a172b100f672d89daeeb0247e017fc6394083f9b
Bryn <bschulha@ualberta.ca>
2020-09-16 02:14:23 +0000

    Used Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("😡😡😡 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
   } else if (actual === expected) {
-    console.log("💚💚💚 Assertion Passed: " + actual + " === " + expected);
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
   }
-};
+ };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
