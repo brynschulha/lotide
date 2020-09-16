b515d6bb4b9c8cd466fd258838b15e2a64ac0353
Bryn <bschulha@ualberta.ca>
2020-09-16 22:03:48 +0000

    Made a func to show which index each letter has in a given sentence

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  }
+ };
+
+ const letterPositions = function(sentence) {
+  const results = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if(sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+letterPositions ("lighthouse in the house");
+
+letterPositions ("bryn is so cool");
+
+assertArraysEqual(letterPositions("hello").e, [1]);
