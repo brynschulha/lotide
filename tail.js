4069bcf0dc4ed8aaf98393a0f024d1d9e809a978
Bryn <bschulha@ualberta.ca>
2020-09-21 20:53:20 +0000

    changed the tail function back

--- tail.js
+++ tail.js
@@ -1,12 +1,10 @@
 const tail = function(someArray) {
   let tailArray = [];
   for (let i = 1; i < someArray.length; i++) {
-    if (i === someArray.length - 1) {
       tailArray.push(someArray[i]);
-    }
   }
   console.log(tailArray);
-  return tailArray[0];
+  return tailArray;
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
-  it("returns 'Labs' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), 'Labs');
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
   });
 });
