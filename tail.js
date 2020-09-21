dd1814ed9d2c0436894e6fafa1f87293a1aac6d8
Bryn <bschulha@ualberta.ca>
2020-09-21 20:49:23 +0000

    Changed tail and middle tests to use Mocha and Chai

--- tail.js
+++ tail.js
@@ -1,9 +1,12 @@
 const tail = function(someArray) {
   let tailArray = [];
   for (let i = 1; i < someArray.length; i++) {
-    tailArray.push(someArray[i]);
+    if (i === someArray.length - 1) {
+      tailArray.push(someArray[i]);
+    }
   }
   console.log(tailArray);
+  return tailArray[0];
 };
 
 module.exports = tail;
