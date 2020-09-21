e4491c437d31047a7cd27c7d8416f26406654ff3
Bryn <bschulha@ualberta.ca>
2020-09-21 16:16:16 +0000

    Added a test file for tail

--- tail.js
+++ tail.js
@@ -6,22 +6,4 @@ const tail = function(someArray) {
   console.log(tailArray);
 };
 
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-// TEST CODE
-
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-
-
+module.exports = tail;
