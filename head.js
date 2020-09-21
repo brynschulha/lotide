2c2f090a5eba219e501fdf81f7d1716cd96fdfb6
Bryn <bschulha@ualberta.ca>
2020-09-21 16:06:49 +0000

    Created separate test files using require

--- head.js
+++ head.js
@@ -3,21 +3,4 @@ const head = function (someArray) {
   return(first);
 }
 
-
-
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
-  }
- };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
